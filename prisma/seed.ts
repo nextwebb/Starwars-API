bb63563a23d39ac1896fab25b59b22124d4ea51e
System Administrator <root@Petersons-MacBook-Pro.local>
2021-05-24 02:23:55 +0100

    updated node engine

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,32 +1,32 @@
-import { name, random } from 'faker';
-// files
-import prisma from '../src/services/prismaClient';
-let timeout: NodeJS.Timeout;
+// import { name, random } from 'faker';
+// // files
+// import prisma from '../src/services/prismaClient';
+// let timeout: NodeJS.Timeout;
 
-// A `seed` function so that we can use async/await
-async function seed(num: number) {
-  for (let i = 0; i < num; i++) {
-    timeout = setTimeout(async () => {
-      const user = await prisma.comment.create({
-        data: {
-          content: name.findName(),
-          movie: random.movie(70),
+// // A `seed` function so that we can use async/await
+// async function seed(num: number) {
+//   for (let i = 0; i < num; i++) {
+//     timeout = setTimeout(async () => {
+//       const user = await prisma.comment.create({
+//         data: {
+//           content: name.findName(),
+//           movie: random.movie(70),
 
-        },
-      });
+//         },
+//       });
 
-      console.log(`new user created`, user.id);
-    }, 1000);
-  }
-}
+//       console.log(`new user created`, user.id);
+//     }, 1000);
+//   }
+// }
 
-seed(10)
-  .catch((e) => {
-    clearTimeout(timeout);
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(async () => {
-    clearTimeout(timeout);
-    await prisma.$disconnect();
-  });
+// seed(10)
+//   .catch((e) => {
+//     clearTimeout(timeout);
+//     console.error(e);
+//     process.exit(1);
+//   })
+//   .finally(async () => {
+//     clearTimeout(timeout);
+//     await prisma.$disconnect();
+//   });
