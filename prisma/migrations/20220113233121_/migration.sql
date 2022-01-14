c5e88f92d039bf368d582254c59298a72e681a66
peterson oaikhenah <oaikhenahpeterson@gmail.com>
2022-01-14 03:49:25 +0100

    BREAKING CHANGE: re-run migrations

--- prisma/migrations/20220113233121_/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Changed the type of `episodeId` on the `Comment` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
-
-*/
--- AlterTable
-ALTER TABLE "Comment" DROP COLUMN "episodeId",
-ADD COLUMN     "episodeId" INTEGER NOT NULL;
--- prisma/migrations/20220114024840_/migration.sql
+++ prisma/migrations/20220114024840_/migration.sql
@@ -2,6 +2,7 @@
 CREATE TABLE "Comment" (
     "id" SERIAL NOT NULL,
     "content" TEXT NOT NULL,
+    "episodeId" INTEGER NOT NULL,
     "ip" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "movie" TEXT NOT NULL,
